4e30f06a57af4ee8f8da464605e498e531b4b3ad
Francisco Guerrero <aguerrero@pivotal.io>
2020-10-01 15:51:47 -0700

    Error out when impersonation is enabled for the FILE protocol

--- server/pxf-hdfs/src/main/java/org/greenplum/pxf/plugins/hdfs/HcfsType.java
+++ server/pxf-hdfs/src/main/java/org/greenplum/pxf/plugins/hdfs/HcfsType.java
@@ -6,6 +6,7 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.mapreduce.MRJobConfig;
 import org.apache.parquet.hadoop.metadata.CompressionCodecName;
 import org.greenplum.pxf.api.model.RequestContext;
+import org.greenplum.pxf.api.security.SecureLogin;
 import org.greenplum.pxf.api.utilities.Utilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,6 +26,15 @@ public enum HcfsType {
         }
     },
     FILE {
+        @Override
+        protected String getDataUriForPrefix(Configuration configuration, String dataSource, String scheme) {
+            if (SecureLogin.isUserImpersonationEnabled(configuration)) {
+                throw new IllegalArgumentException("impersonation is not supported when writing to file. " +
+                        "Set the impersonation value to false for the 'pxf.service.user.impersonation' property in your 'pxf-site.xml' configuration.");
+            }
+            return super.getDataUriForPrefix(configuration, dataSource, scheme);
+        }
+
         @Override
         protected String validateAndNormalizeBasePath(String basePath) {
             if (StringUtils.isBlank(basePath))
